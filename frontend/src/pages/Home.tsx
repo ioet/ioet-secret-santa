064a0e890fb2d0fcbd20a218239dc21541b7c6cb
listerineh <jsalvarez4@espe.edu.ec>
2022-11-26 17:36:56 -0500

    fix: layout

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -6,6 +6,7 @@ import RegistrationForm from "../components/RegistrationForm";
 import renderer from "../components/Renderer";
 import SecretSanta from "../components/SecretSanta";
 import envManager from "../config/envManager";
+import Image from ".././assets/background_santa_image.png"
 
 interface Props {
   isRegistered: boolean,
@@ -17,11 +18,68 @@ const Home = ({ isRegistered, setIsRegistered }: Props) => {
   const [gameDeadline] = useState(envManager.GAME_DEADLINE);
 
   return (
-    <Box sx={{ width: '100%', height: '100vh', background: 'linear-gradient(210deg, rgba(255,128,128,1) 1%, rgba(255,0,0,1) 100%);' }}>
-      <Box sx={{ display: 'flex', alignItems: 'center', height: '100vh' }} width={{ sm: '100%', md: '50%' }}>
-        <Box sx={{ display: 'block', width: '100%', textAlign: 'center' }}>
-          <Typography variant="h2" sx={{ color: 'white', p: 5, wordWrap: "break-word" }}>Days until registration ends!</Typography>
+    <Box
+      display={{ sm: 'block', md: 'flex' }}
+      sx={{
+        width: '100%',
+        height: '100vh',
+        background: 'linear-gradient(210deg, rgba(255,128,128,1) 1%, rgba(255,0,0,1) 100%);'
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+        }}
+        width={{
+          sm: '100%',
+          md: '50%'
+        }}
+        height={{
+          sm: '50vh',
+          md: '100vh',
+        }}
+      >
+        <Box
+          sx={{
+            display: 'block',
+            width: '100%',
+            textAlign: 'center',
+          }}
+        >
+          <Typography
+            variant="h2"
+            sx={{
+              color: 'white',
+              p: 5,
+              wordWrap: "break-word"
+            }}
+          >Days until registration ends!</Typography>
           <Button variant="contained">Register</Button>
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+        }}
+        width={{
+          sm: '100%',
+          md: '50%'
+        }}
+        height={{
+          sm: '50vh',
+          md: '100vh',
+        }}
+      >
+        <Box
+          sx={{
+            display: 'block',
+            width: '100%',
+            textAlign: 'center',
+          }}
+        >
+          <Box component="img" src={Image} sx={{ width: '350px' }} />
         </Box>
       </Box>
     </Box>
