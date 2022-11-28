495c5c322c07f56e65c4c99396c1e0fa99c1b4de
Alan1108 <alan.ibm1108@gmail.com>
2022-11-28 11:34:05 -0500

    fix: Added a new variable into frontend terraform stack and created the zone locally, so the pipeline does not break

--- infrastructure/frontend/main.tf
+++ infrastructure/frontend/main.tf
@@ -18,16 +18,23 @@ provider "aws" {
   region = "us-east-1"
 }
 
+locals {
+  zone_name = "secretsanta.ioet.com"
+}
+
 module "secret-santa-website" {
   source                = "git@github.com:ioet/infra-terraform-modules.git//aws-static-website?ref=v0.0.28"
   create_route53_domain = true
   bucket_name           = "ioet-secret-santa"
-  zone_name             = aws_route53_zone.secresanta_zone.name
+  zone_name             = local.zone_name
   website_domain        = "www.secretsanta.ioet.com"
   error_page            = "index.html"
   index_page            = "index.html"
+  depends_on = [
+    aws_route53_zone.secresanta_zone
+  ]
 }
 
 resource "aws_route53_zone" "secresanta_zone" {
-  name = "secretsanta.ioet.com"
+  name = local.zone_name
 }
