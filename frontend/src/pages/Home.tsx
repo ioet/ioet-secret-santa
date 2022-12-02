b52b2a24b0a1cf4d9d3257a136adad768d143944
listerineh <jsalvarez4@espe.edu.ec>
2022-12-02 02:21:42 -0500

    feat: validate registration status of the player

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,8 +1,15 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Registration from './Registration';
 import Admin from './Admin';
 import envManager from "../config/envManager";
 import SecretSanta from './SecretSanta';
+import axios from 'axios';
+
+
+const backend = axios.create({
+  baseURL: envManager.BACKEND_URL,
+  withCredentials: true,
+});
 
 interface Props {
   isRegistered: boolean,
@@ -14,7 +21,22 @@ interface Props {
 
 const Home = ({ isRegistered, setIsRegistered, isAdmin, setIsAdmin, setIsLogged }: Props) => {
   const [registrationDeadline] = useState(envManager.REGISTRATION_DEADLINE);
-  const [gameDeadline] = useState(envManager.GAME_DEADLINE);
+
+  const sessionStorageData = sessionStorage.getItem('user');
+  const jsonData = sessionStorageData && JSON.parse(sessionStorageData);
+
+  const fetchRegistrationStatus = async () => {
+    const response = await backend.get(`/player/get/${jsonData['email']}`);
+    return response.status === 200 ? response.data : null;
+  }
+
+  useEffect(() => {
+    const validateRegistrationStatus = async () => {
+      const status = await fetchRegistrationStatus();
+      status && setIsRegistered(status['is_player_registered'])
+    }
+    validateRegistrationStatus();
+  }, [])
 
   return (
     <>
